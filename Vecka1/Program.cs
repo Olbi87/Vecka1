2833137246e708bc1aae285a598d0add4590bd70
Olbi87 <P_vivar_gonzalez@yahoo.se>
2021-01-19 10:20:43 +0100

    Initial commit

---
+++ Vecka1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vecka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 10;
+            Console.WriteLine(myNumber);
+        }
+    }
+}
